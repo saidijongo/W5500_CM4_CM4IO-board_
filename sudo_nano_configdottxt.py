b9c8b9f77e96063a06a66d4204f94f609d75ecc2
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-11-10 14:08:41 +0900

    Create sudo_nano_configdottxt.py

---
+++ sudo_nano_configdottxt.py
@@ -0,0 +1,41 @@
+  GNU nano 5.4                                                 config.txt *                                                        
+
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=on
+
+# Automatically load overlays for detected cameras
+start_x=1
+
+# Automatically load overlays for detected DSI displays
+display_auto_detect=1
+
+# Enable DRM VC4 V3D driver
+#dtoverlay=vc4-kms-v3d
+max_framebuffers=2
+
+# Run in 64-bit mode
+arm_64bit=1
+
+# Disable compensation for displays with overscan
+disable_overscan=1
+
+[cm4]
+# Enable host mode on the 2711 built-in XHCI USB controller.
+# This line should be removed if the legacy DWC2 controller is required
+# (e.g. for USB device mode) or if USB support is not required.
+otg_mode=1
+
+[all]
+
+[pi4]
+dtoverlay=vc4-fkms-v3d
+# Run as fast as firmware / board allows
+arm_boost=1
+
+[all]
+enable_uart=1
+gpu_mem=128
+
+#Added in Nov 10 2023 for the W5500 SPI ethn protocol. 
+dtoverlay=anyspi,spi0-0,dev="w5500",speed=30000000
+dtoverlay=w5500
